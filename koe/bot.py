3f953967c2e0dcd090a940441938d3d885c68a1c
Luke Yang <lukey.ubc@qq.com>
2022-12-16 23:42:57 -0800

    integrated chatgpt

--- koe/bot.py
+++ koe/bot.py
@@ -1,16 +1,40 @@
-from os import path
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import nonebot
-import bot_config
+from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+
+# Custom your logger
+# 
+# from nonebot.log import logger, default_format
+# logger.add("error.log",
+#            rotation="00:00",
+#            diagnose=False,
+#            level="ERROR",
+#            format=default_format)
+
+# You can pass some keyword args config to init function
+nonebot.init()
+app = nonebot.get_asgi()
+
+driver = nonebot.get_driver()
+driver.register_adapter(ONEBOT_V11Adapter)
+
+nonebot.load_builtin_plugins("echo")
+nonebot.load_plugin('nonebot_plugin_cartoon')
+# nonebot.load_plugin('nonebot_plugin_multi_chatgpt')
+nonebot.load_plugin('nonebot_plugin_chatgpt')
+
+# Please DO NOT modify this file unless you know what you are doing!
+# As an alternative, you should use command `nb` or modify `pyproject.toml` to load plugins
+nonebot.load_from_toml("pyproject.toml")
+
+# Modify some config / config depends on loaded configs
+# 
+# config = driver.config
+# do something...
 
 
-nonebot.init(bot_config)
-# 第一个参数为插件路径，第二个参数为插件前缀（模块的前缀）
-nonebot.load_plugins(path.join(path.dirname(__file__), 'bot_plugins'), 'bot_plugins')
-
-# 如果使用 asgi
-bot = nonebot.get_bot()
-app = bot.asgi
-
-if __name__ == '__main__':
-    nonebot.run()
+if __name__ == "__main__":
+    nonebot.logger.warning("Always use `nb run` to start the bot instead of manually running!")
+    nonebot.run(app="__mp_main__:app")
