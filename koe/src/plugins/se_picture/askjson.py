7eed9a0a713eda91ddad7da86848c43a52aa3056
Luke Yang <lukey.ubc@qq.com>
2023-01-05 21:29:33 -0800

    updated api

--- koe/src/plugins/se_picture/askjson.py
+++ koe/src/plugins/se_picture/askjson.py
@@ -1,12 +1,13 @@
 import httpx
 from nonebot import logger
-
+import asyncio
 
 async def get_url() -> dict:
     """
     :return: 早报图片链接
     """
-    url="https://www.loliapi.com/bg/"
+    url="https://api.1314.cool/img/sort/api/api.php?return=json"
     async with httpx.AsyncClient() as client:
         r = (await client.get(url=url)).json()
+        print(r)
         return r
