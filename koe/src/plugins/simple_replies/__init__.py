7d311e729cf6fc9ea70dfcfb430caebcf79f8d29
Luke Yang <lukey.ubc@qq.com>
2023-01-05 23:52:24 -0800

    disable dev

--- koe/src/plugins/simple_replies/__init__.py
+++ koe/src/plugins/simple_replies/__init__.py
@@ -3,7 +3,7 @@ from nonebot.rule import to_me
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, MessageSegment
 import random
 
-se_pic=on_command("",priority=6,block=True)
+se_pic=on_command("",priority=6,block=False)
 @se_pic.handle()
 async def _(bot:Bot,event:MessageEvent):
     pass
