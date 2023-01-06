57396da6eedd19a95c1542d46971a26f377992d0
Luke Yang <lukey.ubc@qq.com>
2023-01-05 23:36:26 -0800

    pass test

--- koe/src/plugins/simple_replies/__init__.py
+++ koe/src/plugins/simple_replies/__init__.py
@@ -3,14 +3,7 @@ from nonebot.rule import to_me
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, MessageSegment
 import random
 
-se_pic=on_command("来一份色图",aliases={"来一份涩图"},priority=2,block=True)
+se_pic=on_command("来一份色图",aliases={"来一份涩图"},priority=6,block=True)
 @se_pic.handle()
 async def _(bot:Bot,event:MessageEvent):
-    if random.random()>0.2:
-        img_url=(await askjson.get_url())
-        if img_url:
-            await se_pic.send(message=MessageSegment.image(img_url))
-        else:
-            logger.info('获取时出现错误')
-    else:
-        await se_pic.send(message='不行，不可以涩涩')
+    pass
