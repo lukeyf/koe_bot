4dada9d561b403c5bcc796fda72a4ab6233cd888
Luke Yang <lukey.ubc@qq.com>
2023-01-05 21:55:29 -0800

    fix bug

--- koe/src/plugins/se_picture/askjson.py
+++ koe/src/plugins/se_picture/askjson.py
@@ -9,7 +9,7 @@ async def get_url() -> str:
     url="https://www.loliapi.com/acg/pc/?type=json"
     async with httpx.AsyncClient() as client:
         r = (await client.get(url=url))
-        r = r[r.text.find('https'),r.text.find('.jpg')+4]
+        r = r.text[r.text.find('https'),r.text.find('.jpg')+4]
         return r
 if __name__ == '__main__':
     asyncio.run(get_url())
