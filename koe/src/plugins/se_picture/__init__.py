b0fda10659c75d479fdd6167808d3664ef07f353
Luke Yang <lukey.ubc@qq.com>
2023-01-05 23:41:21 -0800

    update more replies

--- koe/src/plugins/se_picture/__init__.py
+++ koe/src/plugins/se_picture/__init__.py
@@ -8,9 +8,10 @@ REPLY = [
         "没有，有也不给",
         "天天色图色图的，今天就把你变成色图！",
         "咱没有色图",
-        "哈？你的脑子一天都在想些什么呢，咱才没有这种东西啦。"
+        "哈？你的脑子一天都在想些什么呢，咱才没有这种东西啦。",
+        "不行，不可以涩涩！"
     ]
-se_pic=on_command("色图",aliases={"涩图"},priority=2,block=True)
+se_pic=on_command("来一份色图",aliases={"来一份涩图"},priority=2,block=True)
 @se_pic.handle()
 async def _(bot:Bot,event:MessageEvent):
     if random.random()>0.3:
--- koe/src/plugins/simple_replies/__init__.py
+++ koe/src/plugins/simple_replies/__init__.py
@@ -3,7 +3,7 @@ from nonebot.rule import to_me
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, MessageSegment
 import random
 
-se_pic=on_command("来一份色图",aliases={"来一份涩图"},priority=6,block=True)
+se_pic=on_command("",priority=6,block=True)
 @se_pic.handle()
 async def _(bot:Bot,event:MessageEvent):
     pass
