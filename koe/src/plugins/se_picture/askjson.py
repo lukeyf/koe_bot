924410be3fec79ddb7ec883c84068c9125cfdfe7
Luke Yang <lukey.ubc@qq.com>
2023-01-05 21:30:35 -0800

    updated api

--- koe/src/plugins/se_picture/askjson.py
+++ koe/src/plugins/se_picture/askjson.py
@@ -9,5 +9,4 @@ async def get_url() -> dict:
     url="https://api.1314.cool/img/sort/api/api.php?return=json"
     async with httpx.AsyncClient() as client:
         r = (await client.get(url=url)).json()
-        print(r)
         return r
